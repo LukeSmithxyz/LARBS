db972bd9f64849c04c8578503999d4ff340e6cbd
Luke <luke@lukesmith.xyz>
2017-10-05 02:55:09 -0700

    chroot pacman needs no confirm

--- chroot.sh
+++ chroot.sh
@@ -10,11 +10,11 @@ locale-gen
 
 #echo "test" > /etc/hostname
 
-pacman -S networkmanager
+pacman --noconfirm --needed -S networkmanager
 systemctl enable NetworkManager
 systemctl start NetworkManager
 
-pacman -S grub && grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
+pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
 
 larbs() { curl -O http://lukesmith.xyz/larbs/root.sh && root.sh ;}
 dialog --title "Install Luke's Rice" --yesno "This install script will easily let you access Luke's Auto-Rice Boostrapping Scripts (LARBS) which automatically install a full Arch Linux i3-gaps desktop environment.\n\nIf you'd like to install this, select yes, otherwise select no.\n\nLuke"  15 60 && larbs
