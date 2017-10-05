dba06bf0379eff989cd661a8321ce2b8823bf8a5
Luke <luke@lukesmith.xyz>
2017-10-05 03:05:08 -0700

    chroot fix, root.sh was not running

--- chroot.sh
+++ chroot.sh
@@ -8,8 +8,6 @@ echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
 locale-gen
 
-#echo "test" > /etc/hostname
-
 pacman --noconfirm --needed -S networkmanager
 systemctl enable NetworkManager
 systemctl start NetworkManager
@@ -17,5 +15,5 @@ systemctl start NetworkManager
 pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
 
 pacman --noconfirm --needed -S dialog
-larbs() { curl -O http://lukesmith.xyz/larbs/root.sh && root.sh ;}
+larbs() { curl -O http://lukesmith.xyz/larbs/root.sh && bash root.sh ;}
 dialog --title "Install Luke's Rice" --yesno "This install script will easily let you access Luke's Auto-Rice Boostrapping Scripts (LARBS) which automatically install a full Arch Linux i3-gaps desktop environment.\n\nIf you'd like to install this, select yes, otherwise select no.\n\nLuke"  15 60 && larbs
