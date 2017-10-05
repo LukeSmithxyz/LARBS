d2b3c1d39ce1a8a81fcd3fc7033fda4317105a47
Luke <luke@lukesmith.xyz>
2017-10-05 00:12:16 -0700

    NetMan addition

--- chroot.sh
+++ chroot.sh
@@ -10,6 +10,10 @@ locale-gen
 
 echo "test" > /etc/hostname
 
+pacman -S networkmanager
+systemctl enable NetworkManager
+systemctl start NetworkManager
+
 pacman -S grub && grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
 
 curl -O http://lukesmith.xyz/larbs/root.sh && root.sh
