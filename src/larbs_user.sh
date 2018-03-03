9e6a174e936549ce867e67cf0aad2b645f9e5ba4
Luke Smith <luke@lukesmith.xyz>
2018-03-02 22:08:00 -0700

    user redirects to null

--- src/larbs_user.sh
+++ src/larbs_user.sh
@@ -24,7 +24,7 @@ eth=$(ls /sys/class/net | grep eth)
 sed -e "s/wlp3s0/$wifi/g; s/enp0s25/$eth/g" /home/$(whoami)/.config/polybar/config /home/$(whoami)/.bashrc
 
 dialog --infobox "Installing \"packer\", an AUR helper..." 10 60
-aurcheck packer >/dev/tty6
+aurcheck packer >/dev/null
 
 count=$(cat /tmp/aur_queue | wc -l)
 n=0
@@ -33,28 +33,28 @@ for prog in $(cat /tmp/aur_queue)
 do
 	n=$((n+1))
 	dialog --infobox "Downloading and installing program $n out of $count: $prog..." 10 60
-	aurcheck $prog >/dev/tty6
+	aurcheck $prog >/dev/null
 done
 
 echo Downloading config files...
-git clone https://github.com/lukesmithxyz/voidrice.git >/dev/tty6 &&
-	rsync -va voidrice/ /home/$(whoami) >/dev/tty6 &&
-	rm -rf voidrice >/dev/tty6
+git clone https://github.com/lukesmithxyz/voidrice.git >/dev/null &&
+	rsync -va voidrice/ /home/$(whoami) >/dev/null &&
+	rm -rf voidrice >/dev/null
 
 echo Downloading email setup...
-git clone https://github.com/lukesmithxyz/mutt-wizard.git /home/$(whoami)/.config/mutt >/dev/tty6
+git clone https://github.com/lukesmithxyz/mutt-wizard.git /home/$(whoami)/.config/mutt >/dev/null
 
 dialog --infobox "Generating bash/ranger/qutebrowser shortcuts..." 4 60
-git clone https://github.com/LukeSmithxyz/shortcut-sync.git >/dev/tty6 &&
-	rsync shortcut-sync/shortcuts.sh ~/.scripts/ >/dev/tty6 &&
-	rsync shortcut-sync/folders ~/.scripts/ >/dev/tty6 &&
-	rsync shortcut-sync/configs ~/.scripts/ >/dev/tty6 &&
-	bash /home/$(whoami)/.scripts/shortcuts.sh >/dev/tty6 &&
-	rm -rf shortcut-sync/ >/dev/tty6
+git clone https://github.com/LukeSmithxyz/shortcut-sync.git >/dev/null &&
+	rsync shortcut-sync/shortcuts.sh ~/.scripts/ >/dev/null &&
+	rsync shortcut-sync/folders ~/.scripts/ >/dev/null &&
+	rsync shortcut-sync/configs ~/.scripts/ >/dev/null &&
+	bash /home/$(whoami)/.scripts/shortcuts.sh >/dev/null &&
+	rm -rf shortcut-sync/ >/dev/null
 
 dialog --infobox "Preparing welcome message..." 4 50
 curl https://raw.githubusercontent.com/LukeSmithxyz/larbs/master/src/welcome_i3 >> /home/$(whoami)/.config/i3/config
 
 dialog --infobox "Reseting Pulseaudio..." 4 50
-killall pulseaudio >/dev/tty6
-pulseaudio --start >/dev/tty6
+killall pulseaudio >/dev/null
+pulseaudio --start >/dev/null
