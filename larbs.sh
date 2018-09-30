32d22b120fac8f0a0716532bac70cf2704c03a92
Luke Smith <luke@lukesmith.xyz>
2018-09-30 14:27:03 -0400

    color fix

--- larbs.sh
+++ larbs.sh
@@ -211,7 +211,7 @@ systembeepoff
 newperms "%wheel ALL=(ALL) ALL\\n%wheel ALL=(ALL) NOPASSWD: /usr/bin/shutdown,/usr/bin/reboot,/usr/bin/systemctl suspend,/usr/bin/wifi-menu,/usr/bin/mount,/usr/bin/umount,/usr/bin/pacman -Syu,/usr/bin/pacman -Syyu,/usr/bin/packer -Syu,/usr/bin/packer -Syyu,/usr/bin/systemctl restart NetworkManager,/usr/bin/rc-service NetworkManager restart,/usr/bin/pacman -Syyu --noconfirm,/usr/bin/loadkeys,/usr/bin/yay"
 
 # Make pacman and yay colorful because why not.
-sed -i "s/#Color^/Color/g" /etc/pacman.conf
+sed -i "s/^#Color/Color/g" /etc/pacman.conf
 
 # Last message! Install complete!
 finalize
