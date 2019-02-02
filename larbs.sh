eb20829c3d325bff5f8614498b6981f13c3c1bef
Luke Smith <luke@lukesmith.xyz>
2019-02-02 07:01:13 -0500

    xurls added

--- larbs.sh
+++ larbs.sh
@@ -117,7 +117,7 @@ putgitrepo() { # Downlods a gitrepo $1 and places the files in $2 only overwriti
 	[ ! -d "$2" ] && mkdir "$2" && chown -R "$name:wheel" "$2"
 	chown -R "$name:wheel" "$dir"
 	sudo -u "$name" git clone --depth 1 "$1" "$dir/gitrepo" >/dev/null 2>&1 &&
-	sudo -u "$name" cp -rfT "$dir"/gitrepo "$2"
+	sudo -u "$name" cp -rfT "$dir/gitrepo" "$2"
 	}
 
 serviceinit() { for service in "$@"; do
