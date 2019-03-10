7606c120a63b87afa76b7446792ef1ef5cfb047d
Luke Smith <luke@lukesmith.xyz>
2019-03-09 20:04:36 -0500

    LANG fix in autoinstall

--- testing/chroot.sh
+++ testing/chroot.sh
@@ -8,6 +8,7 @@ ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
 
 hwclock --systohc
 
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
 locale-gen
