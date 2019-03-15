c080941ab47583bb19ef3f9519694edcfe76ab47
Luke Smith <luke@lukesmith.xyz>
2019-03-15 16:34:52 -0400

    force

--- larbs.sh
+++ larbs.sh
@@ -198,7 +198,7 @@ installationloop
 
 # Install the dotfiles in the user's home directory
 putgitrepo "$dotfilesrepo" "/home/$name"
-rm "/home/$name/README.md" "/home/$name/LICENSE"
+rm -f "/home/$name/README.md" "/home/$name/LICENSE"
 
 # Install the LARBS Firefox profile in ~/.mozilla/firefox/
 putgitrepo "https://github.com/LukeSmithxyz/mozillarbs.git" "/home/$name/.mozilla/firefox"
