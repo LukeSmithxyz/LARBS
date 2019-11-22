ebe38c505f35974ee7867df6b94afc489d9400a2
Luke Smith <luke@lukesmith.xyz>
2019-11-22 18:58:11 -0500

    firefox build removed

--- larbs.sh
+++ larbs.sh
@@ -209,9 +209,6 @@ installationloop
 putgitrepo "$dotfilesrepo" "/home/$name" "$repobranch"
 rm -f "/home/$name/README.md" "/home/$name/LICENSE"
 
-# Install the LARBS Firefox profile in ~/.mozilla/firefox/
-putgitrepo "https://github.com/LukeSmithxyz/mozillarbs.git" "/home/$name/.mozilla/firefox"
-
 # Enable services here.
 serviceinitwrap NetworkManager cronie
 
