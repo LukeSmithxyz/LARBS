7f533ff8ad11a60f886213792c2625ed7ad45562
Jim Barrett <barrettj1971@gmail.com>
2020-03-27 08:23:43 -0400

    screenlayouts live elsewhere.

--- archi3/larbs.sh
+++ archi3/larbs.sh
@@ -209,8 +209,6 @@ mkdir "/home/$name/.cache/connections/ovpn"
 touch "/home/$name/.cache/connections/mysql"
 touch "/home/$name/.cache/connections/openvpn"
 touch "/home/$name/.cache/connections/ssh"
-mkdir "/home/$name/.cache/screenlayouts"
-touch "/home/$name/.cache/screenlayouts/config"
 
 # Pulseaudio, if/when initially installed, often needs a restart to work immediately.
 [ -f /usr/bin/pulseaudio ] && resetpulse
