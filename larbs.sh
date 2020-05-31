723ab12146aa34447fe4459fe7c08a778343fff3
Vlad Doster <mvdoster@gmail.com>
2020-05-31 15:39:13 -0500

    this change gets rid of bash dotfiles being created
    if bash, you get a .bashrc, .bash_history and .bash_profile on new installl

--- larbs.sh
+++ larbs.sh
@@ -71,7 +71,7 @@ preinstallmsg() { \
 adduserandpass() { \
 	# Adds user `$name` with password $pass1.
 	dialog --infobox "Adding user \"$name\"..." 4 50
-	useradd -m -g wheel -s /bin/bash "$name" >/dev/null 2>&1 ||
+	useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
 	usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
 	repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel $(dirname "$repodir")
 	echo "$name:$pass1" | chpasswd
