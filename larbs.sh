1d8b03595abbff088d79c0dc22d3e43b128dcb97
Luke Smith <luke@lukesmith.xyz>
2020-10-11 15:27:51 -0400

    turn on tap to click by default. fix #118

--- larbs.sh
+++ larbs.sh
@@ -219,6 +219,16 @@ sudo -u "$name" mkdir -p "/home/$name/.cache/zsh/"
 # dbus UUID must be generated for Artix runit.
 dbus-uuidgen > /var/lib/dbus/machine-id
 
+# Tap to click
+[ ! -f /etc/X11/xorg.conf.d/40-libinput.conf ] && echo "Section "InputClass"
+        Identifier "libinput touchpad catchall"
+        MatchIsTouchpad "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "libinput"
+	# Enable left mouse button by tapping
+	Option "Tapping" "on"
+EndSection" > /etc/X11/xorg.conf.d/40-libinput.conf
+
 # Fix fluidsynth/pulseaudio issue.
 grep -q "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" /etc/conf.d/fluidsynth ||
 	echo "OTHER_OPTS='-a pulseaudio -m alsa_seq -r 48000'" >> /etc/conf.d/fluidsynth
