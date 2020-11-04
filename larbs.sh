cc6966b6d94b229e82765b21ff96a8594bbd37e1
Luke Smith <luke@lukesmith.xyz>
2020-11-04 14:31:00 -0500

    dumb

--- larbs.sh
+++ larbs.sh
@@ -170,7 +170,7 @@ preinstallmsg || error "User exited."
 refreshkeys || error "Error automatically refreshing Arch keyring. Consider doing so manually."
 
 for x in curl base-devel git ntp zsh; do
-	dialog --title "LARBS Installation" --infobox "Installing \`x\` which is required to install and configure other programs." 5 70
+	dialog --title "LARBS Installation" --infobox "Installing \`$x\` which is required to install and configure other programs." 5 70
 	installpkg "$x"
 done
 
