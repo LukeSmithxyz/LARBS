2f335c3c2e3799b5a8499f9de235a57cf60c4194
anntnzrb <anntnzrb@protonmail.com>
2021-08-24 09:39:23 -0500

    Log errors to stderr

--- larbs.sh
+++ larbs.sh
@@ -23,7 +23,7 @@ esac done
 
 installpkg(){ pacman --noconfirm --needed -S "$1" >/dev/null 2>&1 ;}
 
-error() { echo "ERROR: $1" ; exit 1;}
+error() { printf "%s\n" "$1" >&2; exit 1; }
 
 welcomemsg() { \
 	dialog --title "Welcome!" --msgbox "Welcome to Luke's Auto-Rice Bootstrapping Script!\\n\\nThis script will automatically install a fully-featured Linux desktop, which I use as my main machine.\\n\\n-Luke" 10 60
