301638626982cddf90f6cf99043f4e5c7ee78ece
elwolf <elwolf6@protonmail.com>
2021-09-15 20:17:09 -0400

    Update chroot.sh

--- testing/chroot.sh
+++ testing/chroot.sh
@@ -1,17 +1,20 @@
 #Potential variables: timezone, lang and local
 
-passwd
-
-TZuser=$(cat tzfinal.tmp)
-
 ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
 
 hwclock --systohc
 
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US ISO-8859-1" >> /etc/locale.gen
 locale-gen
+
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+
+mkinitcpio -P
+
+passwd
+
+TZuser=$(cat tzfinal.tmp)
 
 pacman --noconfirm --needed -S networkmanager
 systemctl enable NetworkManager
