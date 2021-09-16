70daf2ab31835f7848484918f20593ab429c93b1
elwolf <elwolf6@protonmail.com>
2021-09-15 20:12:21 -0400

    Update chroot.sh

--- testing/chroot.sh
+++ testing/chroot.sh
@@ -17,8 +17,8 @@ pacman --noconfirm --needed -S networkmanager
 systemctl enable NetworkManager
 systemctl start NetworkManager
 
-pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/sda && grub-mkconfig -o /boot/grub/grub.cfg
+pacman --noconfirm --needed -S grub efibootmgr && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB && grub-mkconfig -o /boot/grub/grub.cfg
 
 pacman --noconfirm --needed -S dialog
-larbs() { curl -O https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/larbs.sh && bash larbs.sh ;}
-dialog --title "Install Luke's Rice" --yesno "This install script will easily let you access Luke's Auto-Rice Boostrapping Scripts (LARBS) which automatically install a full Arch Linux i3-gaps desktop environment.\n\nIf you'd like to install this, select yes, otherwise select no.\n\nLuke"  15 60 && larbs
+larbs() { curl -O https://raw.githubusercontent.com/elwolf6/LARBS/master/larbs.sh ;}
+dialog --title "Install elwolf's Rice" --yesno "This install script will easily let you access elwolf's Auto-Rice Boostrapping Scripts which automatically install a full Arch Linux nord dwm desktop environment.\n\nIf you'd like to install this, select yes, otherwise select no.\n\elwolf"  15 60 && larbs
