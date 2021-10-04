896865ba4014fe5414c10f2fb3e5114f09d1e2f6
elwolf <elwolf6@protonmail.com>
2021-10-04 03:29:12 +0000

    Update chroot-runit.sh

--- testing/chroot-runit.sh
+++ testing/chroot-runit.sh
@@ -19,17 +19,6 @@ EOF
 
 pacman --noconfirm --needed -S networkmanager-runit grub efibootmgr && grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB && grub-mkconfig -o /boot/grub/grub.cfg
 
-NetworkManager
-ckb-next-daemon &
-
-ln -s /etc/runit/sv/NetworkManager /run/runit/service
-sv up NetworkManager
-mkdir -p /etc/runit/sv/ckb-next-daemon
-printf '#!/bin/sh
-exec ckb-next-daemon > /dev/null 2>&1' > /etc/runit/sv/ckb-next-daemon/run
-ln -s /etc/runit/sv/ckb-next-daemon /run/runit/service
-sv up ckb-next-daemon
-
 pacman --noconfirm --needed -S dialog git
-larbs() { curl -O https://raw.githubusercontent.com/elwolf6/LARBS/master/larbs.sh && bash larbs.sh;}
+larbs() { curl -O https://raw.githubusercontent.com/elwolf6/LARBS/master/larbs-runit.sh && bash larbs-runit.sh;}
 dialog --title "Install elwolf's Rice" --yesno "This install script will easily let you access elwolf's Auto-Rice Boostrapping Scripts which automatically install a full Arch Linux nord dwm desktop environment.\n\nIf you'd like to install this, select yes, otherwise select no.\n\elwolf"  15 60 && larbs
