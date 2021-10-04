872e16cd9c10fe2fd289b3fc5338715dcc6f96a1
elwolf <elwolf6@protonmail.com>
2021-10-03 22:19:21 -0400

    Update chroot-runit.sh

--- testing/chroot-runit.sh
+++ testing/chroot-runit.sh
@@ -22,8 +22,8 @@ pacman --noconfirm --needed -S networkmanager-runit grub efibootmgr && grub-inst
 ln -s /etc/runit/sv/NetworkManager /run/runit/service
 sv up NetworkManager
 mkdir -p /etc/runit/sv/ckb-next-daemon
-echo "#!/bin/sh
-exec ckb-next-daemon > /dev/null 2>&1" >> /etc/runit/sv/ckb-next-daemon/run
+printf '#!/bin/sh
+exec ckb-next-daemon > /dev/null 2>&1' > /etc/runit/sv/ckb-next-daemon/run
 ln -s /etc/runit/sv/ckb-next-daemon /run/runit/service
 sv up ckb-next-daemon
 
