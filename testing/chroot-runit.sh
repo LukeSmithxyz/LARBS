d9f666d663635073e7c90c9857debf07152e9c65
elwolf <elwolf6@protonmail.com>
2021-10-03 22:23:17 -0400

    Update chroot-runit.sh

--- testing/chroot-runit.sh
+++ testing/chroot-runit.sh
@@ -28,5 +28,5 @@ ln -s /etc/runit/sv/ckb-next-daemon /run/runit/service
 sv up ckb-next-daemon
 
 pacman --noconfirm --needed -S dialog git
-larbs() { curl -O https://raw.githubusercontent.com/elwolf6/LARBS/master/larbs-runit.sh && bash larbs-runit.sh;}
+larbs() { curl -O https://raw.githubusercontent.com/elwolf6/LARBS/master/larbs.sh && bash larbs-runit.sh;}
 dialog --title "Install elwolf's Rice" --yesno "This install script will easily let you access elwolf's Auto-Rice Boostrapping Scripts which automatically install a full Arch Linux nord dwm desktop environment.\n\nIf you'd like to install this, select yes, otherwise select no.\n\elwolf"  15 60 && larbs
