52b079cc736e7872435d8782fa4fa2e2d7aed03a
elwolf <elwolf6@protonmail.com>
2021-11-09 21:24:10 -0500

    Update larbs-runit.sh

--- larbs-runit.sh
+++ larbs-runit.sh
@@ -319,21 +319,6 @@ newperms "%wheel ALL=(ALL) ALL #LARBS
 sudo -u "$name" git config --global user.name elwolf6
 sudo -u "$name" git config --global user.email elwolf6@protonmail.com
 
-NetworkManager
-
-mkdir -p /run/runit
-mkdir -p /run/runit/service
-
-ln -s /etc/runit/sv/NetworkManager /run/runit/service
-sv up NetworkManager
-mkdir -p /etc/runit/sv/ckb-next-daemon
-printf '#!/bin/sh
-exec ckb-next-daemon > /dev/null 2>&1' > /etc/runit/sv/ckb-next-daemon/run
-chmod +x /etc/runit/sv/ckb-next-daemon/run
-ckb-next-daemon > /dev/null 2>&1 && killall ckb-next-daemon
-ln -s /etc/runit/sv/ckb-next-daemon /run/runit/service
-sv up ckb-next-daemon
-
 # Last message! Install complete!
 finalize
 clear
