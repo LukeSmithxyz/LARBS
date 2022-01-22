7c6e686236566fbc183b2198f272c9df0a16f25f
Luke Smith <luke@lukesmith.xyz>
2022-01-21 19:56:29 -0500

    fix #408

--- larbs.sh
+++ larbs.sh
@@ -57,7 +57,7 @@ preinstallmsg() { \
 adduserandpass() { \
 	# Adds user `$name` with password $pass1.
 	dialog --infobox "Adding user \"$name\"..." 4 50
-	useradd -m -g wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
+	useradd -m -G wheel -s /bin/zsh "$name" >/dev/null 2>&1 ||
 	usermod -a -G wheel "$name" && mkdir -p /home/"$name" && chown "$name":wheel /home/"$name"
 	export repodir="/home/$name/.local/src"; mkdir -p "$repodir"; chown -R "$name":wheel "$(dirname "$repodir")"
 	echo "$name:$pass1" | chpasswd
@@ -78,7 +78,7 @@ refreshkeys() { \
 Include = /etc/pacman.d/mirrorlist-arch" >> /etc/pacman.conf
 			done
 			pacman -Sy >/dev/null 2>&1
-			pacman-key --populate archlinux
+			pacman-key --populate archlinux >/dev/null 2>&1
 			;;
 	esac ;}
 
