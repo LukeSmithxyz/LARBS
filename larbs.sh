c0a523308a124c188e8f5cb2ef136032d6262fd6
Luke Smith <luke@lukesmith.xyz>
2022-06-28 09:23:53 -0400

    remove command line options entirely! close #405, close #450

--- larbs.sh
+++ larbs.sh
@@ -1,23 +1,15 @@
 #!/bin/sh
+
 # Luke's Auto Rice Boostrapping Script (LARBS)
 # by Luke Smith <luke@lukesmith.xyz>
 # License: GNU GPLv3
 
 ### OPTIONS AND VARIABLES ###
 
-while getopts ":a:r:b:p:h" o; do case "${o}" in
-	h) printf "Optional arguments for custom use:\\n  -r: Dotfiles repository (local file or url)\\n  -p: Dependencies and programs csv (local file or url)\\n  -a: AUR helper (must have pacman-like syntax)\\n  -h: Show this message\\n" && exit 1 ;;
-	r) dotfilesrepo=${OPTARG} && git ls-remote "$dotfilesrepo" || exit 1 ;;
-	b) repobranch=${OPTARG} ;;
-	p) progsfile=${OPTARG} ;;
-	a) aurhelper=${OPTARG} ;;
-	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
-esac done
-
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-[ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
-[ -z "$aurhelper" ] && aurhelper="yay"
-[ -z "$repobranch" ] && repobranch="master"
+dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
+progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+aurhelper="yay"
+repobranch="master"
 
 ### FUNCTIONS ###
 
