b309a606a10ff56f6157a394004f65c922769839
Jocomol <jocomol@protonmail.com>
2023-08-26 18:45:16 +0200

    Added list_to_progs.sh and programms I want to have installed

---
+++ list_to_progs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Provide a list of packages you want to have installed in a file named install.txt where every line is a package.
+aur=$(pacman -Qm)
+while read package; do
+	desc=$(pacman -Q --info $package | grep Description | cut -d ":" -f2) || true
+	desc="${desc:1}"
+	flag=""
+	if pacman -Qm $package &> /dev/null; then
+		flag=A
+	fi
+	echo $flag,$package,\"$desc.\" >> static/procs.csv
+done < install.txt
